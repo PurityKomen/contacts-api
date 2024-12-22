9da2355a5413ac0e7ff72b1760e2116ab073dcd0
PurityKomen <jepkorirpurity8@gmail.com>
2024-12-22 13:27:11 +0300

    added basic files

---
+++ server.js
@@ -0,0 +1,23 @@
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to the port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
